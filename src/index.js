fca9d28ecdd28b92d9403a95d9f2cc54c0ffb11c
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-05-23 18:56:33 -0600

    Break
    Taking a break for school. I got the API working with a chrome extension, will have to learn C# to get this operational in a production environment.

--- src/index.js
+++ src/index.js
@@ -10,12 +10,27 @@ $(document).ready(function(){
     const url = `http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=ac29a956585544ffac8892ff962934a7&mapid=41020&outputType=JSON`;
 
     request.onreadystatechange = function(){
-
-      console.log(this.readyState);
-
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
     };
     request.open("GET", url, true);
     request.send();
+
+    function getElements(response){
+      $('.staNm').text(`${response.ctatt.eta[1].staNm}`);
+      $('.rn').text(`${response.ctatt.eta[1].rn}`);
+
+      const now = new Date();
+      const arrival = parseInt(response.ctatt.eta[1].arrT[14] + response.ctatt.eta[1].arrT[15]);
+      
+      console.log(typeof arrival);
+      console.log(now);
+
+      //$('.arrT').text(`${}`);
+      
+    }
   });
   
 });
